8a475733de6fb003a9b383a64f3d1ceb3225f93a
SheetalJade2019 <sheetal.jade93@gmail.com>
2023-08-05 07:04:11 +0530

    logger added

--- project/app/views.py
+++ project/app/views.py
@@ -1,91 +1,78 @@
-from django.shortcuts import render, redirect
+# from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login,logout, authenticate
+# from django.contrib.auth import login,logout, authenticate
 from django.conf import settings
 from django.core.mail import send_mail
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from app.models import *
 from datetime import date
-from .seed import seed_data
+# from .seed import seed_data
 from datetime import datetime
 # seed_data()
 from datetime import date
 # from .task import add
 from celery import shared_task
-  
-  
+from celery.utils.log import get_logger
+clogger = get_logger(__name__)
+
+# from project.logfun import logger  
+
+
+# function to test Celery
 @shared_task
 def add(x, y):
+    clogger.info(f"------------------------add function : shared task info--------{x+y}")
     return x + y
 
 
-@api_view(['GET', 'POST'])
-def send_event_email(request):
-    pass
+@shared_task
+def send_event_mail():
+    try:
+        # print("Check todays event")
+        objs = event_details.objects.filter(date=date.today())
+        clogger.info(f"No of todays events : {len(objs)}")
+        for i in objs:
+            # print("EVENT DETAILS : ",i.event_name, i.user_id.email, i.template_id)
+            # print("USER DETAILS : ", i.user_id.username, i.user_id.email)
+            # print("Template : ",i.template_id.template)
 
-#1 check event_details table for event
-def check_event():
-    print("check_event() called")
-    objs = event_details.objects.filter(date=date.today())
-    print("No of todays events : ",len(objs))
-    for i in objs:
-        print("EVENT DETAILS : ",i.event_name, i.user_id.email, i.template_id)
-        # get email id of bday person
-        print("USER DETAILS : ", i.user_id.username, i.user_id.email)
-        # get template
-        print("Template : ",i.template_id.template)
-        # event_template.objects.get(template_id=)
-        # send email
-        subject = f"{i.event_name}"
-        message = f"{i.template_id.template}"
-        email_from = settings.EMAIL_HOST_USER
-        recipient_list = [i.user_id.email, ]
-        send_mail( subject, message, email_from, recipient_list )
-        print(f"Email Sent To : {i.user_id.email}")
-    # pass
+            # send email
+            subject = f"{i.event_name}"
+            message = f"{i.template_id.template}"
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [i.user_id.email, ]
+            send_mail( subject, message, email_from, recipient_list )
+            clogger.info(f"Email Sent To : {i.user_id.email}")
+            clogger.info(f"* * * * * * * * automated mail sent to {i.user_id.email}  at {datetime.now()} * * * * * * * * * * *")
 
-#2 draft mail by referring template
-def draft_mail():
+        # pass
+    except Exception as e:
+        clogger.error(f"Exception : {str(e)}")
+
+"""def background_proc():
+    # send_event_mail.apply_async( countdown=180,retry_policy={'max_retries': 3}) 
+    send_event_mail.apply_async( countdown=10) 
+
+background_proc()
+"""
+
+# call shared task in function (background)
+def celery_check():
     # run the task in 5 seconds
-    result = add.apply_async((2, 3), countdown=60) 
-    print("# # # # # # # RESULT :",result, datetime.now())
     
-draft_mail()
-
-#3 send mail
-def try_send_mail(subject, message,email_from, recipient_list):
     try:
-        # subject = sub#'welcome to Automated Email ..!'
-        # message = msg#f'Hi Sheetal, Email sent successfully'
-        email_from = settings.EMAIL_HOST_USER
-        recipient_list = [settings.EMAIL_HOST_USER, ]
-        send_mail( subject, message, email_from, recipient_list )
-        print("sending mail: Success")
+        # add.delay(2, 2)
+        # data = send_event_mail.apply_async(countdown=10)
+        # print("# # # # # data # # # ",data)
+        result = add.apply_async((2, 3), countdown=10) 
+        clogger.info(f"# # # # # # # RESULT :{result, datetime.now()}")#retry_policy={'max_retries': 3}
+        # clogger.info(f"# # # # # data # # # {data}")
+        clogger.info(f"# # # # # # # RESULT : {result.get(), datetime.now()}")
+        
+    except Exception as exc:
+        clogger.exception('-----------------------------Sending task raised: %r', exc)
+        clogger.error('-----------------------------Sending task raised: %r', exc)
 
-    except Exception as e :
-        print("Exception", str(e))   
-# try_send_mail()
+celery_check()
 
-def add_user(uname, pwd, email):
-    try:
-        User.objects.create(username=uname, password=pwd, email=email)
-    except Exception as e:
-        print("Exception while adding user : ", str(e))
-
-def add_event(event_name, user_id, template_id):
-    try:
-        event_details.objects.create(event_name,user_id=user_id,template_id=template_id, date=date)
-    except Exception as e:
-        print("Exception while adding event : ", str(e))
-
-def add_template( event_type, template):
-    try:
-        event_template.objects.create(event_type=event_type, template=template)
-    except Exception as e:
-        print("Exception while adding template : ", str(e))
-
-# add_user("SJade", "SJade", "sheetal.jade93@gmail.com")
-# add_event("BIRTHDAY", 1, 1)
-# add_template("birthday","HAPPY BIRTHDAY..!")
-# check_event()
